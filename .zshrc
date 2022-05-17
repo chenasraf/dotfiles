ad5dc5bcb48fa37f5102d8bcd1897758dcb3ed78
Chen Asraf <chenasrafil@gmail.com>
2022-05-17 22:41:27 +0300

    cleanups

--- .zshrc
+++ .zshrc
@@ -4,6 +4,3 @@ source $HOME/.dotfiles/aliases.sh
 source $HOME/.dotfiles/sources.sh
 source $HOME/.dotfiles/home.sh
 source $HOME/.dotfiles/scripts/java.sh
-
-if [[ -f "$HOME/.dotfiles/_local.sh" ]]; then source "$HOME/.dotfiles/_local.sh"; fi
-if [[ -f "$HOME/.iterm2_shell_integration.zsh" ]]; then source "$HOME/.iterm2_shell_integration.zsh"; fi
--- sources.sh
+++ sources.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh || :
-[ -f /opt/homebrew/opt/chruby/share/chruby/chruby.sh ] && source /opt/homebrew/opt/chruby/share/chruby/chruby.sh || :
+if [[ -f "$HOME/.dotfiles/_local.sh" ]]; then source "$HOME/.dotfiles/_local.sh"; fi
+if [[ -f "$HOME/.iterm2_shell_integration.zsh" ]]; then source "$HOME/.iterm2_shell_integration.zsh"; fi
+if [[ -f ~/.fzf.zsh ]]; then source ~/.fzf.zsh; fi
+if [[ -f /opt/homebrew/opt/chruby/share/chruby/chruby.sh ]]; then source /opt/homebrew/opt/chruby/share/chruby/chruby.sh; fi
