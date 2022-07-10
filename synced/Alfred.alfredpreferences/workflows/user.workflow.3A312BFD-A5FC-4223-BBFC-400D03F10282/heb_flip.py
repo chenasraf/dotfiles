a22dae864c0d8ff06ca0225cc8b81d7a3c8c12f9
Chen Asraf <chen.asraf@planckresolution.com>
2022-07-10 15:34:48 +0300

    update heb flip workflow :)

--- synced/Alfred.alfredpreferences/workflows/user.workflow.3A312BFD-A5FC-4223-BBFC-400D03F10282/heb_flip.py
+++ synced/Alfred.alfredpreferences/workflows/user.workflow.3A312BFD-A5FC-4223-BBFC-400D03F10282/heb_flip.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import json
 # -*- coding: utf-8 -*-
 
 import sys
@@ -10,13 +11,30 @@ args = sys.argv[1:]
 lang = args[0]
 query = " ".join(args[1:])
 
-res = query.lower()
-lang_map = to_heb_map if lang == "heb" else to_eng_map
-for k, v in lang_map.items():
-    res = res.replace(k, v)
+
+def run(lang, query):
+    res = query.lower()
+    lang_map = to_heb_map if lang == "heb" else to_eng_map
+    for k, v in lang_map.items():
+        res = res.replace(k, v)
+    return res
+
+
+res = run(lang, query)
+
+out = {
+    "items": [
+        {
+            "title": f"Copy {res}",
+            "subtitle": f"Copy {res} to clipboard",
+            "arg": res,
+            "icon": "85B31B09-7486-435A-A0A7-2A83BEE74B85.png" if lang == "heb" else "5EAA025A-A267-432E-9089-0608B2CBE4D3.png",
+        }
+    ]
+}
 
 # sys.stdout.write("args: " + str(args) + "\n")
 # sys.stdout.write("lang: " + str(lang) + "\n")
 # sys.stdout.write("query: " + str(query) + "\n")
 
-sys.stdout.write(res)
+sys.stdout.write(json.dumps(out))
