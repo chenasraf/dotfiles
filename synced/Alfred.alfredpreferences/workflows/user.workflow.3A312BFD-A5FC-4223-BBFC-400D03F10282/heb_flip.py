65cb00687b75e8bf639af34db79cd686f1cc6ce4
Chen Asraf <chen.asraf@planckresolution.com>
2022-07-10 18:13:27 +0300

    update flip workflow

--- synced/Alfred.alfredpreferences/workflows/user.workflow.3A312BFD-A5FC-4223-BBFC-400D03F10282/heb_flip.py
+++ synced/Alfred.alfredpreferences/workflows/user.workflow.3A312BFD-A5FC-4223-BBFC-400D03F10282/heb_flip.py
@@ -11,6 +11,9 @@ args = sys.argv[1:]
 lang = args[0]
 query = " ".join(args[1:])
 
+heb_icon = "85B31B09-7486-435A-A0A7-2A83BEE74B85.png"
+eng_icon = "5EAA025A-A267-432E-9089-0608B2CBE4D3.png"
+
 
 def run(lang, query):
     res = query.lower()
@@ -20,18 +23,29 @@ def run(lang, query):
     return res
 
 
-res = run(lang, query)
+def make_res_item(lang, res):
+    return {
+        "title": f"Copy {res}",
+        "subtitle": f"Copy {res} to clipboard",
+        "arg": [res],
+        "icon": {
+            # "type": "filetype",
+            "path": heb_icon if lang == "heb" else eng_icon,
+        },
+        "copy": res,
+    }
 
-out = {
-    "items": [
-        {
-            "title": f"Copy {res}",
-            "subtitle": f"Copy {res} to clipboard",
-            "arg": res,
-            "icon": "85B31B09-7486-435A-A0A7-2A83BEE74B85.png" if lang == "heb" else "5EAA025A-A267-432E-9089-0608B2CBE4D3.png",
-        }
+
+out = {"items": []}
+
+if lang != "":
+    out['items'] = [make_res_item(run(lang, query))]
+else:
+    out['items'] = [
+        make_res_item("eng", run("eng", query)),
+        make_res_item("heb", run("heb", query)),
     ]
-}
+
 
 # sys.stdout.write("args: " + str(args) + "\n")
 # sys.stdout.write("lang: " + str(lang) + "\n")
