4cc9d196e5169aa29cfc81470cd08441d80f299c
Chen Asraf <chenasrafil@gmail.com>
2022-07-11 02:02:22 +0300

    update flip workflow + use newer python by default

--- synced/Alfred.alfredpreferences/workflows/user.workflow.3A312BFD-A5FC-4223-BBFC-400D03F10282/heb_flip.py
+++ synced/Alfred.alfredpreferences/workflows/user.workflow.3A312BFD-A5FC-4223-BBFC-400D03F10282/heb_flip.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import json
+import re
 # -*- coding: utf-8 -*-
 
 import sys
@@ -13,6 +14,10 @@ query = " ".join(args[1:])
 
 heb_icon = "85B31B09-7486-435A-A0A7-2A83BEE74B85.png"
 eng_icon = "5EAA025A-A267-432E-9089-0608B2CBE4D3.png"
+lang_names = {
+    "heb": "Hebrew",
+    "eng": "English",
+}
 
 
 def run(lang, query):
@@ -25,8 +30,8 @@ def run(lang, query):
 
 def make_res_item(lang, res):
     return {
-        "title": f"Copy {res}",
-        "subtitle": f"Copy {res} to clipboard",
+        "title": f"Flipped '{res}' to {lang_names[lang]}",
+        "subtitle": f"Copy '{res}' to clipboard",
         "arg": [res],
         "icon": {
             # "type": "filetype",
@@ -39,12 +44,15 @@ def make_res_item(lang, res):
 out = {"items": []}
 
 if lang != "":
-    out['items'] = [make_res_item(run(lang, query))]
+    out['items'] = [make_res_item(lang, run(lang, query))]
 else:
     out['items'] = [
         make_res_item("eng", run("eng", query)),
         make_res_item("heb", run("heb", query)),
     ]
+
+if re.match(r'[a-z]', query):
+    out['items'] = out['items'][::-1]
 
 
 # sys.stdout.write("args: " + str(args) + "\n")
