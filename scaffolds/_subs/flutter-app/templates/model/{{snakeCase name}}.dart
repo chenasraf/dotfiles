051f63272f7befcdc7c6503d43a5c94433bfbc1c
Chen Asraf <chenasrafil@gmail.com>
2022-11-08 22:38:52 +0200

    update flutter tpl

--- scaffolds/_subs/flutter-app/templates/model/{{snakeCase name}}.dart
+++ scaffolds/_subs/flutter-app/templates/model/{{snakeCase name}}.dart
@@ -1,11 +1,9 @@
 class {{pascalCase name}} {
-  //
+  {{pascalCase name}}();
 
   factory {{pascalCase name}}.fromJson(Map<String,dynamic> json) => throw UnimplementedError();
 
-  Map<String,dynamic> toJson() {
-    throw UnimplementedError();
-  }
+  Map<String,dynamic> toJson() => throw UnimplementedError();
 
   {{pascalCase name}} copyWith() => throw UnimplementedError();
 }
