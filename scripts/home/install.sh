a6445e882f2a4811313dc09136abbae5bb4ab9a6
Chen Asraf <chenasrafil@gmail.com>
2023-01-25 01:22:23 +0200

    fix: update install script

--- scripts/home/install.sh
+++ scripts/home/install.sh
@@ -71,6 +71,7 @@ echo_cyan "Done"
 echo_cyan "Downloading gi_gen latest version..."
 gi_ver=$(curl -s "https://api.github.com/repos/chenasraf/gi_gen/tags" | jq -r '.[0].name')
 ver_file="$DOTFILES/.bin/.gi_gen_version"
+mkdir -p $(dirname $ver_file)
 touch $ver_file
 existing_ver=$(cat $ver_file)
 if [[ "$existing_ver" != "$gi_ver" ]]; then
