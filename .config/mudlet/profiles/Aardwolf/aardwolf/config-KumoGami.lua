34b310762074ff8683ee8e5664020b9443d24836
Chen Asraf <chenasrafil@gmail.com>
2023-07-24 16:20:18 +0300

    feat: sync mudlet

---
+++ .config/mudlet/profiles/Aardwolf/aardwolf/config-KumoGami.lua
@@ -0,0 +1,2 @@
+return {{[ [[dual]] ] = [[dagger]],[ [[cplvl]] ] = 150,[ [[vpot]] ] = [[refreshing]],[ [[attack1]] ] = [[cast "chill touch"]],[ [[pbag]] ] = [[bag]],[ [[debuff2]] ] = [[dirt]],[ [[tdiff]] ] = -3,[ [[wield]] ] = [[melpomene]],[ [[port]] ] = [[cast "word of recall"]],[ [[mpot]] ] = [[moonlight]],[ [[group]] ] = 1,[ [[debuff1]] ] = [[marbu]],[ [[hpot]] ] = [[elixir]],[ [[cpexp]] ] = 700,[ [[speak]] ] = [[false]],},
+}
