533fbe4d2ece6b93daa31d3ef519382b5f8f0cfd
Chen Asraf <chenasrafil@gmail.com>
2023-07-30 11:30:44 +0300

    feat: add gaard workflow

---
+++ synced/Alfred.alfredpreferences/workflows/user.workflow.55E2EF57-AB9F-45D9-AF04-B505E0D32238/list.mjs
@@ -0,0 +1,515 @@
+export default [{
+  "name": "Aardington Estate",
+  "href": "http://maps.gaardian.com/index.php?areaid=344"
+}, {
+  "name": "Aardwolf Birthday Area",
+  "href": "http://maps.gaardian.com/index.php?areaid=20"
+}, {
+  "name": "Aardwolf Zoological Park",
+  "href": "http://maps.gaardian.com/index.php?areaid=39"
+}, {
+  "name": "Adventures in Sendhia",
+  "href": "http://maps.gaardian.com/index.php?areaid=316"
+}, {
+  "name": "Aerial City of Cineko",
+  "href": "http://maps.gaardian.com/index.php?areaid=350"
+}, {
+  "name": "All in a Fayke Day",
+  "href": "http://maps.gaardian.com/index.php?areaid=53"
+}, {
+  "name": "The Amazon Nation",
+  "href": "http://maps.gaardian.com/index.php?areaid=157"
+}, {
+  "name": "The Amusement Park",
+  "href": "http://maps.gaardian.com/index.php?areaid=27"
+}, {
+  "name": "Ancient Greece",
+  "href": "http://maps.gaardian.com/index.php?areaid=75"
+}, {
+  "name": "Anthrox",
+  "href": "http://maps.gaardian.com/index.php?areaid=125"
+}, {
+  "name": "The Archipelago of Entropy",
+  "href": "http://maps.gaardian.com/index.php?areaid=154"
+}, {
+  "name": "Arisian Realm",
+  "href": "http://maps.gaardian.com/index.php?areaid=174"
+}, {
+  "name": "Art of Melody",
+  "href": "http://maps.gaardian.com/index.php?areaid=9"
+}, {
+  "name": "Ascension Bluff Nursing Home",
+  "href": "http://maps.gaardian.com/index.php?areaid=347"
+}, {
+  "name": "Atlantis",
+  "href": "http://maps.gaardian.com/index.php?areaid=74"
+}, {
+  "name": "The Aylorian Academy",
+  "href": "http://maps.gaardian.com/index.php?areaid=258"
+}, {
+  "name": "The Bard Clan",
+  "href": "http://maps.gaardian.com/index.php?areaid=226"
+}, {
+  "name": "The Blighted Tundra of Andarin",
+  "href": "http://maps.gaardian.com/index.php?areaid=300"
+}, {
+  "name": "Brightsea and Glimmerdim",
+  "href": "http://maps.gaardian.com/index.php?areaid=326"
+}, {
+  "name": "The Call of Heroes",
+  "href": "http://maps.gaardian.com/index.php?areaid=10"
+}, {
+  "name": "Canyon Memorial Hospital",
+  "href": "http://maps.gaardian.com/index.php?areaid=254"
+}, {
+  "name": "Castle Vlad-Shamir",
+  "href": "http://maps.gaardian.com/index.php?areaid=97"
+}, {
+  "name": "Chaprenula's Laboratory",
+  "href": "http://maps.gaardian.com/index.php?areaid=250"
+}, {
+  "name": "The Chasm and The Catacombs",
+  "href": "http://maps.gaardian.com/index.php?areaid=251"
+}, {
+  "name": "The Chessboard",
+  "href": "http://maps.gaardian.com/index.php?areaid=28"
+}, {
+  "name": "Child's Play",
+  "href": "http://maps.gaardian.com/index.php?areaid=30"
+}, {
+  "name": "Christmas Vacation",
+  "href": "http://maps.gaardian.com/index.php?areaid=146"
+}, {
+  "name": "Cloud City of Gnomalin",
+  "href": "http://maps.gaardian.com/index.php?areaid=259"
+}, {
+  "name": "The Coral Kingdom",
+  "href": "http://maps.gaardian.com/index.php?areaid=81"
+}, {
+  "name": "Cradlebrook",
+  "href": "http://maps.gaardian.com/index.php?areaid=80"
+}, {
+  "name": "The Dark Continent, Abend",
+  "href": "http://maps.gaardian.com/index.php?areaid=267"
+}, {
+  "name": "Dark Elf Stronghold",
+  "href": "http://maps.gaardian.com/index.php?areaid=118"
+}, {
+  "name": "The Dark Temple of Zyian",
+  "href": "http://maps.gaardian.com/index.php?areaid=161"
+}, {
+  "name": "The DarkLight",
+  "href": "http://maps.gaardian.com/index.php?areaid=109"
+}, {
+  "name": "The Darkside of the Fractured Lands",
+  "href": "http://maps.gaardian.com/index.php?areaid=91"
+}, {
+  "name": "Death's Manor",
+  "href": "http://maps.gaardian.com/index.php?areaid=72"
+}, {
+  "name": "Den of Thieves",
+  "href": "http://maps.gaardian.com/index.php?areaid=25"
+}, {
+  "name": "Descent to Hell",
+  "href": "http://maps.gaardian.com/index.php?areaid=86"
+}, {
+  "name": "Desert Doom",
+  "href": "http://maps.gaardian.com/index.php?areaid=168"
+}, {
+  "name": "Dhal'Gora Outlands",
+  "href": "http://maps.gaardian.com/index.php?areaid=325"
+}, {
+  "name": "Diamond Soul Revelation",
+  "href": "http://maps.gaardian.com/index.php?areaid=88"
+}, {
+  "name": "Disciples of Hassan Clan Hall",
+  "href": "http://maps.gaardian.com/index.php?areaid=214"
+}, {
+  "name": "Doom and Gloom",
+  "href": "http://maps.gaardian.com/index.php?areaid=60"
+}, {
+  "name": "Dortmund",
+  "href": "http://maps.gaardian.com/index.php?areaid=31"
+}, {
+  "name": "The Dungeon of Doom",
+  "href": "http://maps.gaardian.com/index.php?areaid=209"
+}, {
+  "name": "Dusk Valley",
+  "href": "http://maps.gaardian.com/index.php?areaid=351"
+}, {
+  "name": "Earth Plane 4",
+  "href": "http://maps.gaardian.com/index.php?areaid=98"
+}, {
+  "name": "The Eighteenth Dynasty",
+  "href": "http://maps.gaardian.com/index.php?areaid=149"
+}, {
+  "name": "Elemental Chaos",
+  "href": "http://maps.gaardian.com/index.php?areaid=290"
+}, {
+  "name": "The Emerald Clan HQ",
+  "href": "http://maps.gaardian.com/index.php?areaid=231"
+}, {
+  "name": "The Empire of Talsa",
+  "href": "http://maps.gaardian.com/index.php?areaid=119"
+}, {
+  "name": "Faerie Tales",
+  "href": "http://maps.gaardian.com/index.php?areaid=136"
+}, {
+  "name": "Faerie Tales II",
+  "href": "http://maps.gaardian.com/index.php?areaid=153"
+}, {
+  "name": "Fantasy Fields",
+  "href": "http://maps.gaardian.com/index.php?areaid=33"
+}, {
+  "name": "The Fire Swamp",
+  "href": "http://maps.gaardian.com/index.php?areaid=265"
+}, {
+  "name": "The First Ascent",
+  "href": "http://maps.gaardian.com/index.php?areaid=289"
+}, {
+  "name": "The Forest of Li'Dnesh",
+  "href": "http://maps.gaardian.com/index.php?areaid=5"
+}, {
+  "name": "Fort Terramire",
+  "href": "http://maps.gaardian.com/index.php?areaid=55"
+}, {
+  "name": "The Fractured Lands",
+  "href": "http://maps.gaardian.com/index.php?areaid=73"
+}, {
+  "name": "Gallows Hill",
+  "href": "http://maps.gaardian.com/index.php?areaid=11"
+}, {
+  "name": "The Gauntlet",
+  "href": "http://maps.gaardian.com/index.php?areaid=36"
+}, {
+  "name": "Giant's Pet Store",
+  "href": "http://maps.gaardian.com/index.php?areaid=26"
+}, {
+  "name": "Gilda And The Dragon",
+  "href": "http://maps.gaardian.com/index.php?areaid=148"
+}, {
+  "name": "The Goblin Fortress",
+  "href": "http://maps.gaardian.com/index.php?areaid=104"
+}, {
+  "name": "Gold Rush",
+  "href": "http://maps.gaardian.com/index.php?areaid=83"
+}, {
+  "name": "The Grand City of Aylor",
+  "href": "http://maps.gaardian.com/index.php?areaid=18"
+}, {
+  "name": "The Graveyard",
+  "href": "http://maps.gaardian.com/index.php?areaid=23"
+}, {
+  "name": "The Great Salt Flats",
+  "href": "http://maps.gaardian.com/index.php?areaid=89"
+}, {
+  "name": "Guardian's Spyre of Knowledge",
+  "href": "http://maps.gaardian.com/index.php?areaid=283"
+}, {
+  "name": "Gypsy Caravan",
+  "href": "http://maps.gaardian.com/index.php?areaid=304"
+}, {
+  "name": "The Halls of Vanir",
+  "href": "http://maps.gaardian.com/index.php?areaid=234"
+}, {
+  "name": "Hatchling Aerie",
+  "href": "http://maps.gaardian.com/index.php?areaid=264"
+}, {
+  "name": "Hedgehogs' Paradise",
+  "href": "http://maps.gaardian.com/index.php?areaid=330"
+}, {
+  "name": "Hotel Orlando",
+  "href": "http://maps.gaardian.com/index.php?areaid=12"
+}, {
+  "name": "House of Cards",
+  "href": "http://maps.gaardian.com/index.php?areaid=275"
+}, {
+  "name": "Imagi's Nation",
+  "href": "http://maps.gaardian.com/index.php?areaid=360"
+}, {
+  "name": "The Imperial City of Reme",
+  "href": "http://maps.gaardian.com/index.php?areaid=84"
+}, {
+  "name": "Imperial Nation",
+  "href": "http://maps.gaardian.com/index.php?areaid=112"
+}, {
+  "name": "The Infestation",
+  "href": "http://maps.gaardian.com/index.php?areaid=41"
+}, {
+  "name": "Insanitaria",
+  "href": "http://maps.gaardian.com/index.php?areaid=135"
+}, {
+  "name": "Into the Long Night",
+  "href": "http://maps.gaardian.com/index.php?areaid=141"
+}, {
+  "name": "Intrigues of Times Past",
+  "href": "http://maps.gaardian.com/index.php?areaid=280"
+}, {
+  "name": "Island of Lost Time",
+  "href": "http://maps.gaardian.com/index.php?areaid=127"
+}, {
+  "name": "Jenny's Tavern",
+  "href": "http://maps.gaardian.com/index.php?areaid=101"
+}, {
+  "name": "Jotunheim",
+  "href": "http://maps.gaardian.com/index.php?areaid=307"
+}, {
+  "name": "Jungles of Verume",
+  "href": "http://maps.gaardian.com/index.php?areaid=59"
+}, {
+  "name": "The Keep of Kearvek",
+  "href": "http://maps.gaardian.com/index.php?areaid=282"
+}, {
+  "name": "Keep of the Kobaloi",
+  "href": "http://maps.gaardian.com/index.php?areaid=327"
+}, {
+  "name": "Kerofk",
+  "href": "http://maps.gaardian.com/index.php?areaid=51"
+}, {
+  "name": "Ketu Uplands",
+  "href": "http://maps.gaardian.com/index.php?areaid=266"
+}, {
+  "name": "Kimr's Farm",
+  "href": "http://maps.gaardian.com/index.php?areaid=301"
+}, {
+  "name": "Kingdom of Ahner",
+  "href": "http://maps.gaardian.com/index.php?areaid=76"
+}, {
+  "name": "Kingsholm",
+  "href": "http://maps.gaardian.com/index.php?areaid=306"
+}, {
+  "name": "Kul Tiras",
+  "href": "http://maps.gaardian.com/index.php?areaid=68"
+}, {
+  "name": "The Labyrinth",
+  "href": "http://maps.gaardian.com/index.php?areaid=82"
+}, {
+  "name": "Land of Legend",
+  "href": "http://maps.gaardian.com/index.php?areaid=13"
+}, {
+  "name": "The Land of Oz",
+  "href": "http://maps.gaardian.com/index.php?areaid=94"
+}, {
+  "name": "The Land of the Beer Goblins",
+  "href": "http://maps.gaardian.com/index.php?areaid=15"
+}, {
+  "name": "The Legendary City of Tanelorn",
+  "href": "http://maps.gaardian.com/index.php?areaid=236"
+}, {
+  "name": "Living Mines of Dak'Tai",
+  "href": "http://maps.gaardian.com/index.php?areaid=291"
+}, {
+  "name": "Loqui Clan Area",
+  "href": "http://maps.gaardian.com/index.php?areaid=217"
+}, {
+  "name": "Lowlands Paradise '96",
+  "href": "http://maps.gaardian.com/index.php?areaid=2"
+}, {
+  "name": "The Maelstrom",
+  "href": "http://maps.gaardian.com/index.php?areaid=288"
+}, {
+  "name": "A Magical Hodgepodge",
+  "href": "http://maps.gaardian.com/index.php?areaid=38"
+}, {
+  "name": "The Marshlands of Agroth",
+  "href": "http://maps.gaardian.com/index.php?areaid=144"
+}, {
+  "name": "Midgaardian Publishing House",
+  "href": "http://maps.gaardian.com/index.php?areaid=219"
+}, {
+  "name": "The Misty Shores of Yarr",
+  "href": "http://maps.gaardian.com/index.php?areaid=147"
+}, {
+  "name": "The Monastery",
+  "href": "http://maps.gaardian.com/index.php?areaid=131"
+}, {
+  "name": "Mountain View Estates",
+  "href": "http://maps.gaardian.com/index.php?areaid=294"
+}, {
+  "name": "The Mountains of Desolation",
+  "href": "http://maps.gaardian.com/index.php?areaid=166"
+}, {
+  "name": "Mudwog's Swamp",
+  "href": "http://maps.gaardian.com/index.php?areaid=328"
+}, {
+  "name": "Necromancers' Guild",
+  "href": "http://maps.gaardian.com/index.php?areaid=69"
+}, {
+  "name": "New Thalos",
+  "href": "http://maps.gaardian.com/index.php?areaid=56"
+}, {
+  "name": "Northstar",
+  "href": "http://maps.gaardian.com/index.php?areaid=121"
+}, {
+  "name": "The Old Cathedral",
+  "href": "http://maps.gaardian.com/index.php?areaid=100"
+}, {
+  "name": "Olde Worlde Carnivale",
+  "href": "http://maps.gaardian.com/index.php?areaid=40"
+}, {
+  "name": "Onyx Bazaar",
+  "href": "http://maps.gaardian.com/index.php?areaid=261"
+}, {
+  "name": "The Palace of Song",
+  "href": "http://maps.gaardian.com/index.php?areaid=319"
+}, {
+  "name": "The Path of the Believer",
+  "href": "http://maps.gaardian.com/index.php?areaid=313"
+}, {
+  "name": "A Peaceful Giant Village",
+  "href": "http://maps.gaardian.com/index.php?areaid=164"
+}, {
+  "name": "Plains of Nulan'Boar",
+  "href": "http://maps.gaardian.com/index.php?areaid=278"
+}, {
+  "name": "Pompeii",
+  "href": "http://maps.gaardian.com/index.php?areaid=129"
+}, {
+  "name": "Radiance Woods",
+  "href": "http://maps.gaardian.com/index.php?areaid=355"
+}, {
+  "name": "Raganatittu",
+  "href": "http://maps.gaardian.com/index.php?areaid=90"
+}, {
+  "name": "Realm of Deneria",
+  "href": "http://maps.gaardian.com/index.php?areaid=273"
+}, {
+  "name": "Realm of the Firebird",
+  "href": "http://maps.gaardian.com/index.php?areaid=126"
+}, {
+  "name": "The Realm of the Hawklords",
+  "href": "http://maps.gaardian.com/index.php?areaid=281"
+}, {
+  "name": "Realm of the Sacred Flame",
+  "href": "http://maps.gaardian.com/index.php?areaid=287"
+}, {
+  "name": "Realm of the Zodiac",
+  "href": "http://maps.gaardian.com/index.php?areaid=257"
+}, {
+  "name": "Rosewood Castle",
+  "href": "http://maps.gaardian.com/index.php?areaid=111"
+}, {
+  "name": "The Ruins of Stormhaven",
+  "href": "http://maps.gaardian.com/index.php?areaid=324"
+}, {
+  "name": "Sen'narre Lake",
+  "href": "http://maps.gaardian.com/index.php?areaid=14"
+}, {
+  "name": "The Shadows of Minos",
+  "href": "http://maps.gaardian.com/index.php?areaid=255"
+}, {
+  "name": "Shady Acres Estates",
+  "href": "http://maps.gaardian.com/index.php?areaid=295"
+}, {
+  "name": "Sheila's Cat Sanctuary",
+  "href": "http://maps.gaardian.com/index.php?areaid=279"
+}, {
+  "name": "Sho'aram, Castle in the Sand",
+  "href": "http://maps.gaardian.com/index.php?areaid=277"
+}, {
+  "name": "The Silver Volcano",
+  "href": "http://maps.gaardian.com/index.php?areaid=85"
+}, {
+  "name": "Siren's Oasis Resort",
+  "href": "http://maps.gaardian.com/index.php?areaid=21"
+}, {
+  "name": "The Slaughter House",
+  "href": "http://maps.gaardian.com/index.php?areaid=151"
+}, {
+  "name": "Snuckles Village",
+  "href": "http://maps.gaardian.com/index.php?areaid=122"
+}, {
+  "name": "Storm Mountain",
+  "href": "http://maps.gaardian.com/index.php?areaid=57"
+}, {
+  "name": "Sundered Vale",
+  "href": "http://maps.gaardian.com/index.php?areaid=253"
+}, {
+  "name": "Swordbreaker's Hoard",
+  "href": "http://maps.gaardian.com/index.php?areaid=256"
+}, {
+  "name": "Tairayden Peninsula",
+  "href": "http://maps.gaardian.com/index.php?areaid=331"
+}, {
+  "name": "Tanra'vea",
+  "href": "http://maps.gaardian.com/index.php?areaid=323"
+}, {
+  "name": "The Temple of Shal'indrael",
+  "href": "http://maps.gaardian.com/index.php?areaid=198"
+}, {
+  "name": "The Temple of Shouggoth",
+  "href": "http://maps.gaardian.com/index.php?areaid=62"
+}, {
+  "name": "Thandeld's Conflict",
+  "href": "http://maps.gaardian.com/index.php?areaid=340"
+}, {
+  "name": "The Three Pillars of Diatz",
+  "href": "http://maps.gaardian.com/index.php?areaid=107"
+}, {
+  "name": "Tilule Rehabilitation Clinic",
+  "href": "http://maps.gaardian.com/index.php?areaid=367"
+}, {
+  "name": "Tournament Camps",
+  "href": "http://maps.gaardian.com/index.php?areaid=8"
+}, {
+  "name": "The Tournament of Illoria",
+  "href": "http://maps.gaardian.com/index.php?areaid=113"
+}, {
+  "name": "The Town of Solan",
+  "href": "http://maps.gaardian.com/index.php?areaid=305"
+}, {
+  "name": "The Tree of Life",
+  "href": "http://maps.gaardian.com/index.php?areaid=188"
+}, {
+  "name": "Twilight Hall",
+  "href": "http://maps.gaardian.com/index.php?areaid=249"
+}, {
+  "name": "Umari's Castle",
+  "href": "http://maps.gaardian.com/index.php?areaid=293"
+}, {
+  "name": "The UnderDark",
+  "href": "http://maps.gaardian.com/index.php?areaid=70"
+}, {
+  "name": "The Underground Hall",
+  "href": "http://maps.gaardian.com/index.php?areaid=244"
+}, {
+  "name": "Unearthly Bonds",
+  "href": "http://maps.gaardian.com/index.php?areaid=171"
+}, {
+  "name": "The Uprising",
+  "href": "http://maps.gaardian.com/index.php?areaid=349"
+}, {
+  "name": "Vidblain, the Ever Dark",
+  "href": "http://maps.gaardian.com/index.php?areaid=272"
+}, {
+  "name": "War of the Wizards",
+  "href": "http://maps.gaardian.com/index.php?areaid=46"
+}, {
+  "name": "Warrior's Training Camp",
+  "href": "http://maps.gaardian.com/index.php?areaid=285"
+}, {
+  "name": "Wayward Alehouse",
+  "href": "http://maps.gaardian.com/index.php?areaid=329"
+}, {
+  "name": "Weather Observatory",
+  "href": "http://maps.gaardian.com/index.php?areaid=299"
+}, {
+  "name": "Wedded Bliss",
+  "href": "http://maps.gaardian.com/index.php?areaid=124"
+}, {
+  "name": "Wildwood",
+  "href": "http://maps.gaardian.com/index.php?areaid=317"
+}, {
+  "name": "Winterlands",
+  "href": "http://maps.gaardian.com/index.php?areaid=175"
+}, {
+  "name": "The Witches of Omen Tor",
+  "href": "http://maps.gaardian.com/index.php?areaid=322"
+}, {
+  "name": "The Wobbly Woes of Woobleville",
+  "href": "http://maps.gaardian.com/index.php?areaid=92"
+}, {
+  "name": "The Wood Elves of Nalondir",
+  "href": "http://maps.gaardian.com/index.php?areaid=37"
+}]
+
