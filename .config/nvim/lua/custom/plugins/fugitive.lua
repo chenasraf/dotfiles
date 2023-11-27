f00b8d9398d475374e2170eeb8d6988c876ee328
Chen Asraf <casraf@pm.me>
2023-11-27 14:12:20 +0200

    feat(fugitive): push all unpushed motion

--- .config/nvim/lua/custom/plugins/fugitive.lua
+++ .config/nvim/lua/custom/plugins/fugitive.lua
@@ -16,27 +16,37 @@ autocmd("BufWinEnter", {
 
     local bufnr = vim.api.nvim_get_current_buf()
     local opts = { buffer = bufnr, remap = false }
+    local function optc(desc)
+      return vim.tbl_extend("force", opts, { desc = desc })
+    end
     -- print("great success", vim.bo.ft, bufnr, vim.inspect(opts))
-    -- vim.keymap.set("n", "gp", "]]k", opts)
+    vim.keymap.set("n", "<leader>gp", function()
+      local current_branch = vim.fn.FugitiveHead()
+
+      local cmd = ":Git push origin @:refs/heads/" .. current_branch
+      local esc_cmd = vim.api.nvim_replace_termcodes(cmd, true, true, true)
+      vim.api.nvim_feedkeys(esc_cmd, "n", true)
+    end, optc("[fugitive] [P]ush all unpushed commits"))
 
     -- rebase always
     vim.keymap.set("n", "<leader>gl", function()
       vim.cmd [[ Git pull --rebase ]]
-    end, opts)
+    end, optc("[fugitive] pu[l]l with rebase"))
 
     vim.keymap.set("n", "<leader>gaa", function()
       vim.cmd [[ Git add . ]]
-    end, opts)
+    end, optc("[fugitive] [a]dd all"))
 
     -- push with upstream
     vim.keymap.set("n", "<leader>gup", function()
       local current_branch = vim.fn.FugitiveHead()
 
       local cmd = ":Git push -u origin " .. current_branch
-      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(cmd, true, true, true), "n", true)
-    end, opts);
+      local esc_cmd = vim.api.nvim_replace_termcodes(cmd, true, true, true)
+      vim.api.nvim_feedkeys(esc_cmd, "n", true)
+    end, optc("[fugitive] [u]pstream push"))
 
-    vim.keymap.set("n", "<leader>gf", ":Git fetch<CR>", opts)
+    vim.keymap.set("n", "<leader>gf", ":Git fetch<CR>", optc("[fugitive] [f]etch"))
   end,
 })
 
