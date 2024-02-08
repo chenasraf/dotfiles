a33e5a60eecb2f7a6502c031d296597d72cac284
Chen Asraf <casraf@pm.me>
2024-02-08 02:23:36 +0200

    fix: resize

--- .config/nvim/lua/custom/plugins/lazygit.lua
+++ .config/nvim/lua/custom/plugins/lazygit.lua
@@ -1,5 +1,8 @@
 return {
-  "kdheepak/lazygit.nvim",
+  dir = "/Users/Dev/lazygit.nvim",
+
+  branch = "feat/resize",
+  -- "kdheepak/lazygit.nvim",
   -- optional for floating window border decoration
   dependencies = {
     "nvim-lua/plenary.nvim",
