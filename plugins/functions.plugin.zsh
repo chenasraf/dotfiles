3bad7e0b93c90f5291cdbee8f9d6b6bec9cfbf7c
Chen Asraf <casraf@pm.me>
2024-07-22 11:04:49 +0300

    feat: set-pnpm-pkg-version cmd

--- plugins/functions.plugin.zsh
+++ plugins/functions.plugin.zsh
@@ -627,6 +627,25 @@ mdp() {
   ($SHELL -c "sleep 3; rm $f; exit 0" &)
 }
 
+# sets pnpm version on closest package.json to current version
+set-pnpm-pkg-version() {
+  fl=$(find-up package.json)
+  if [[ -z $fl ]]; then
+    echo_red "No package.json found"
+    return 1
+  fi
+
+  jq -e '.packageManager' $fl NUL
+  existing=$(echo "$?")
+  if [[ $existing -eq 0 ]]; then
+    if ask "pnpm version already exists. Overwrite?"; then
+      jq '.packageManager = $version' --arg version "pnpm@$(pnpm -v)" $fl >$fl.tmp && mv $fl.tmp $fl
+    fi
+  else
+    jq '.packageManager = $version' --arg version "pnpm@$(pnpm -v)" $fl >$fl.tmp && mv $fl.tmp $fl
+  fi
+}
+
 # select random element from arguments
 # always keep last, breaks syntax highlighting
 randarg() {
