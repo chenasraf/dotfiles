cc82665ed5d4257f8891eadf7903657bdbc4f2cd
Chen Asraf <casraf@pm.me>
2024-09-26 14:31:14 +0300

    feat: update barbar/wezterm keymappings, add dotenvx aliases

--- .config/nvim/lua/custom/plugins/barbar.lua
+++ .config/nvim/lua/custom/plugins/barbar.lua
@@ -13,28 +13,30 @@ return {
   config = function()
     require('barbar').setup()
     -- Move to previous/next
-    vim.keymap.set('n', '≤', '<Cmd>BufferPrevious<CR>')
-    vim.keymap.set('n', '≥', '<Cmd>BufferNext<CR>')
+    vim.keymap.set({ 'n', 'v' }, '<A-[>', '<Cmd>BufferPrevious<CR>')
+    vim.keymap.set({ 'n', 'v' }, '<A-]>', '<Cmd>BufferNext<CR>')
     -- Re-order to previous/next
-    vim.keymap.set('n', '¯', '<Cmd>BufferMovePrevious<CR>')
-    vim.keymap.set('n', '˘', '<Cmd>BufferMoveNext<CR>')
+    vim.keymap.set({ 'n', 'v' }, '<A-{>', '<Cmd>BufferMovePrevious<CR>')
+    vim.keymap.set({ 'n', 'v' }, '<A-}>', '<Cmd>BufferMoveNext<CR>')
     -- Goto buffer in position...
-    vim.keymap.set('n', '¡', '<Cmd>BufferGoto 1<CR>')
-    vim.keymap.set('n', '™', '<Cmd>BufferGoto 2<CR>')
-    vim.keymap.set('n', '£', '<Cmd>BufferGoto 3<CR>')
-    vim.keymap.set('n', '¢', '<Cmd>BufferGoto 4<CR>')
-    vim.keymap.set('n', '∞', '<Cmd>BufferGoto 5<CR>')
-    vim.keymap.set('n', '§', '<Cmd>BufferGoto 6<CR>')
-    vim.keymap.set('n', '¶', '<Cmd>BufferGoto 7<CR>')
-    vim.keymap.set('n', '•', '<Cmd>BufferGoto 8<CR>')
-    vim.keymap.set('n', 'ª', '<Cmd>BufferGoto 9<CR>')
-    vim.keymap.set('n', 'º', '<Cmd>BufferLast<CR>')
+    -- vim.keymap.set('n', '¡', '<Cmd>BufferGoto 1<CR>')
+    -- vim.keymap.set('n', '™', '<Cmd>BufferGoto 2<CR>')
+    -- vim.keymap.set('n', '£', '<Cmd>BufferGoto 3<CR>')
+    -- vim.keymap.set('n', '¢', '<Cmd>BufferGoto 4<CR>')
+    -- vim.keymap.set('n', '∞', '<Cmd>BufferGoto 5<CR>')
+    -- vim.keymap.set('n', '§', '<Cmd>BufferGoto 6<CR>')
+    -- vim.keymap.set('n', '¶', '<Cmd>BufferGoto 7<CR>')
+    -- vim.keymap.set('n', '•', '<Cmd>BufferGoto 8<CR>')
+    -- vim.keymap.set('n', 'ª', '<Cmd>BufferGoto 9<CR>')
+    -- vim.keymap.set('n', 'º', '<Cmd>BufferLast<CR>')
     -- Pin/unpin buffer
-    vim.keymap.set('n', 'π', '<Cmd>BufferPin<CR>')
+    vim.keymap.set('n', '<A-p>', '<Cmd>BufferPin<CR>')
     -- Close buffer
-    vim.keymap.set('n', 'ç', '<Cmd>BufferClose<CR>')
+    vim.keymap.set('n', '<A-c>', '<Cmd>BufferClose<CR>')
     -- Close all but current
-    vim.keymap.set('n', 'Ç', '<Cmd>BufferCloseAllButCurrentOrPinned<CR>')
+    vim.keymap.set('n', '<A-C>', '<Cmd>BufferCloseAllButCurrentOrPinned<CR>')
+    -- Magic buffer-picking mode
+    vim.keymap.set('n', '<C-p>', '<Cmd>BufferPick<CR>')
     -- Wipeout buffer
     --                 :BufferWipeout
     -- Close commands
@@ -43,13 +45,11 @@ return {
     --                 :BufferCloseAllButCurrentOrPinned
     --                 :BufferCloseBuffersLeft
     --                 :BufferCloseBuffersRight
-    -- Magic buffer-picking mode
-    vim.keymap.set('n', '<C-p>', '<Cmd>BufferPick<CR>')
     -- Sort automatically by...
-    vim.keymap.set('n', '<leader>bn', '<Cmd>BufferOrderByBufferNumber<CR>')
-    vim.keymap.set('n', '<leader>bd', '<Cmd>BufferOrderByDirectory<CR>')
-    vim.keymap.set('n', '<leader>bl', '<Cmd>BufferOrderByLanguage<CR>')
-    vim.keymap.set('n', '<leader>bw', '<Cmd>BufferOrderByWindowNumber<CR>')
+    -- vim.keymap.set('n', '<leader>bn', '<Cmd>BufferOrderByBufferNumber<CR>')
+    -- vim.keymap.set('n', '<leader>bd', '<Cmd>BufferOrderByDirectory<CR>')
+    -- vim.keymap.set('n', '<leader>bl', '<Cmd>BufferOrderByLanguage<CR>')
+    -- vim.keymap.set('n', '<leader>bw', '<Cmd>BufferOrderByWindowNumber<CR>')
   end,
-  version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  -- version = '^1.0.0', -- optional: only update when a new 1.x version is released
 }
