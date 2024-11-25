2e0b3d78b2a7af938a8dbefc415ac02435169034
Chen Asraf <casraf@pm.me>
2024-11-25 17:24:45 +0200

    feat(nvim): catppuccin theme

--- .config/nvim/lua/custom/plugins/theme.lua
+++ .config/nvim/lua/custom/plugins/theme.lua
@@ -1,15 +1,29 @@
 return {
-  -- Theme inspired by Atom
-  'navarasu/onedark.nvim',
+  "catppuccin/nvim",
+  name = "catppuccin",
+  lazy = false,
   priority = 1000,
-  config = function()
-    require('onedark').setup({
-      transparent = true,
-      lualine = {
-        transparent = true,
-      },
-      style = 'darker',
-    })
-    vim.cmd.colorscheme 'onedark'
+  opts = {
+    flavor = "mocha",
+    transparent_background = true,
+  },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
+    vim.cmd.colorscheme("catppuccin")
   end,
 }
+-- return {
+--   -- Theme inspired by Atom
+--   'navarasu/onedark.nvim',
+--   priority = 1000,
+--   config = function()
+--     require('onedark').setup({
+--       transparent = true,
+--       lualine = {
+--         transparent = true,
+--       },
+--       style = 'darker',
+--     })
+--     vim.cmd.colorscheme 'onedark'
+--   end,
+-- }
