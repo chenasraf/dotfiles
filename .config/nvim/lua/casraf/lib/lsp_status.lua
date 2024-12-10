4587f9c3aab98cff7a26dea19a8a5425cabf044a
Chen Asraf <c-chen.asraf@appliedsystems.com>
2024-12-10 18:25:07 +0200

    fix(lsp_status): update deprecation

--- .config/nvim/lua/casraf/lib/lsp_status.lua
+++ .config/nvim/lua/casraf/lib/lsp_status.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.lsp_supported = function()
   local bufnr = vim.api.nvim_get_current_buf()
-  local clients = vim.lsp.get_active_clients({ bufnr = bufnr })
+  local clients = vim.lsp.get_clients({ bufnr = bufnr })
   if not clients then
     return '× LSP'
   end
