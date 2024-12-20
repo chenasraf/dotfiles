b22a5ca73919d4f047393a7e125a80fdcd34bbc2
Chen Asraf <casraf@pm.me>
2024-12-20 21:22:57 +0000

    feat: nc aio debug alias

--- plugins/nextcloud.plugin.zsh
+++ plugins/nextcloud.plugin.zsh
@@ -4,5 +4,7 @@ nc-dev-logs() {
 
 alias nc-aio="sudo docker exec --user www-data -it nextcloud-aio-nextcloud"
 alias nc-aio-occ="nc-aio php occ"
+alias nc-aio-debug="nc-aio-occ config:system:set debug --type bool --value"
+
 alias nc-dev="docker exec --user www-data -it master-nextcloud-1"
 alias nc-dev-occ="nc-dev php occ"
