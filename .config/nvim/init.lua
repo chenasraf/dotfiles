40761ade4907bb5fe34f666b78d28d9a40cc26ce
Chen Asraf <c-chen.asraf@appliedsystems.com>
2025-06-03 14:24:23 +0300

    docs: update doc comment

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -125,7 +125,7 @@ require('which-key').add({
 })
 
 -- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
+-- See `:help vim.hl.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
