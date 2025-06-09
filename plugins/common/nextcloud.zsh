0df3f83a41f60d7cbc1dfb79f68e7729832d9831
Chen Asraf <casraf@pm.me>
2025-06-09 13:56:24 +0300

    fix(nextcloud): update alias to use new container name

--- plugins/common/nextcloud.zsh
+++ plugins/common/nextcloud.zsh
@@ -2,9 +2,10 @@ nc-dev-logs() {
   nc-dev tail $@ /var/www/html/data/nextcloud.log
 }
 
+alias nc-dev-start="pushd \$HOME/Dev/nextcloud-docker-dev && docker compose up -d nextcloud && popd"
 alias nc-aio="sudo docker exec --user www-data -it nextcloud-aio-nextcloud"
 alias nc-aio-occ="nc-aio php occ"
 alias nc-aio-debug="nc-aio-occ config:system:set debug --type bool --value"
 
-alias nc-dev="docker exec --user www-data -it master-nextcloud-1"
+alias nc-dev="docker exec --user www-data -it nextcloud-dev-nextcloud-1"
 alias nc-dev-occ="nc-dev php occ"
