0198f6419bc7f317a3abefd3a47474cef9878cfc
Chen Asraf <casraf@pm.me>
2025-09-08 01:43:24 +0300

    chore: cleanup nextcloud plugin

--- plugins/common/nextcloud.zsh
+++ plugins/common/nextcloud.zsh
@@ -1,7 +1,3 @@
-nc-dev-logs() {
-  nc-dev tail $@ /var/www/html/data/nextcloud.log
-}
-
 alias nc-dev-start="pushd \$HOME/Dev/nextcloud-docker-dev && docker compose up -d nextcloud && popd"
 alias nc-aio="sudo docker exec --user www-data -it nextcloud-aio-nextcloud"
 alias nc-aio-occ="nc-aio php occ"
@@ -9,3 +5,7 @@ alias nc-aio-debug="nc-aio-occ config:system:set debug --type bool --value"
 
 alias nc-dev="docker exec --user www-data -it nextcloud-dev-nextcloud-1"
 alias nc-dev-occ="nc-dev php occ"
+
+nc-dev-logs() {
+  nc-dev tail $@ /var/www/html/data/nextcloud.log
+}
