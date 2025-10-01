6edae7834e5e2e4ad866ceea45df56b5c5f620bf
Chen Asraf <casraf@pm.me>
2025-10-01 23:37:00 +0300

    fix(nvim): schemastore nvim 0.11 compat

--- .config/nvim/lua/casraf/plugins/schemastore.lua
+++ .config/nvim/lua/casraf/plugins/schemastore.lua
@@ -1,13 +1,13 @@
 return {
   "b0o/schemastore.nvim",
   config = function()
-    vim.lsp.config.jsonls.setup {
+    vim.lsp.config("jsonls", {
       settings = {
         json = {
           schemas = require('schemastore').json.schemas(),
           validate = { enable = true },
         },
       },
-    }
+    })
   end
 }
