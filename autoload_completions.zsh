2fa27dee5e182900960cbfcccdc1d93c2d4a9ae1
Chen Asraf <casraf@pm.me>
2025-10-05 01:02:26 +0300

    fix: autoload completions

--- autoload_completions.zsh
+++ autoload_completions.zsh
@@ -3,10 +3,8 @@ autoload_completions() {
   autoload bashcompinit
   bashcompinit
   autoload -Uz compinit
-  # autocompletions
-  for i in $(ls $DOTFILES/completions); do
-    autoload $i
-  done
+  # Add completions directory to fpath
+  fpath=($DOTFILES/completions $fpath)
   compinit
 }
 
