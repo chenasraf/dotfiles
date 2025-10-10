455659a15c1a71d693968eae77847409a4dbf7d0
Chen Asraf <casraf@pm.me>
2025-10-11 00:48:07 +0300

    feat(tx): prj save

--- utils/eslint.config.mjs
+++ utils/eslint.config.mjs
@@ -4,6 +4,9 @@ import tseslint from 'typescript-eslint'
 export default [
   ...tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended),
   {
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
     ignores: ['node_modules/', 'build/', 'dist/', 'gen/'],
   },
 ]
