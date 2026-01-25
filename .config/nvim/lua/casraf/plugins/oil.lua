78cacb9459f7d2284e705cca5dd98cbdb15e2175
Chen Asraf <casraf@pm.me>
2026-01-25 11:34:28 +0200

    fix(oil): remap keys to avoid navigation conflicts

--- .config/nvim/lua/casraf/plugins/oil.lua
+++ .config/nvim/lua/casraf/plugins/oil.lua
@@ -1,14 +1,21 @@
 return {
   'stevearc/oil.nvim',
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
     view_options = {
       show_hidden = true,
-    }
+    },
+    keymaps = {
+      -- Remap defaults that conflict with tmux navigation
+      ["<C-h>"] = false, -- was toggle hidden
+      ["<C-l>"] = false, -- was refresh
+      ["g."] = "actions.toggle_hidden",
+      ["<C-r>"] = "actions.refresh",
+    },
   },
-  -- Optional dependencies
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function(opts)
-    require("oil").setup(opts.opts)
+  config = function(_, opts)
+    require("oil").setup(opts)
     vim.keymap.set("n", "-", ":Oil<CR>", { desc = "[Oil] Back to parent dir", silent = true })
   end,
 }
