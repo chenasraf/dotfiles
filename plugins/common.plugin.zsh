174d300579329f40778c45410e9de60e1d53d40f
Chen Asraf <casraf@pm.me>
2026-02-01 10:55:07 +0200

    feat(tx): update aliases, named layouts

--- plugins/common.plugin.zsh
+++ plugins/common.plugin.zsh
@@ -7,15 +7,3 @@ dir="${0:A:h}"
 for file in $dir/common/*.zsh; do
   source $file
 done
-
-# Functions
-
-# kill tmux session by name, or running session
-trm() {
-  sess=$1
-  if [[ -z $sess ]]; then
-    tmux kill-session
-    return $?
-  fi
-  tmux kill-session -t $sess
-}
