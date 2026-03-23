a62802f19b5aba62f02ec2cee81a12581b039266
Chen Asraf <casraf@pm.me>
2026-03-24 00:54:45 +0200

    feat: disable ollama local plugin

--- plugins/loader.zsh
+++ plugins/loader.zsh
@@ -3,7 +3,7 @@
 load_plugins() {
   setopt +o nomatch
   for plugin in ~/.local/share/zsh/plugins/**/*.plugin.zsh; do
-    [ -e "$plugin" ] && source "$plugin"
+    [[ -e "$plugin" && "$plugin" != *.disabled.zsh ]] && source "$plugin"
   done
   source ~/.local/share/zsh/plugins/powerlevel10k/powerlevel10k.zsh-theme
   setopt -o nomatch
