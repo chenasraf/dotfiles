1a21ba9e7fbfce977c3fdc47d510c048bf0920db
Chen Asraf <casraf@pm.me>
2026-04-06 13:36:07 +0300

    feat(aliases): add cc alias

--- aliases.zsh
+++ aliases.zsh
@@ -218,3 +218,4 @@ fbcurl() {
 alias fdg="fd --glob"
 alias mdf="prettier --config \$HOME/.prettierrc --ignore-path \$HOME/.prettierignore --write '**/*.md'"
 alias wands="alias G 'wand --wand-file' G -v wands"
+alias cc="claude"
