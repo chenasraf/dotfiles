f47047afdf0002205364c7b6a71f8a33dec85f7f
Chen Asraf <casraf@pm.me>
2026-04-13 17:15:23 +0300

    fix: tmux navigation inside nvim->lazygit

--- .config/nvim/lua/casraf/plugins/vim-tmux-navigator.lua
+++ .config/nvim/lua/casraf/plugins/vim-tmux-navigator.lua
@@ -1,3 +1,16 @@
+vim.api.nvim_create_autocmd('FileType', {
+  group = vim.api.nvim_create_augroup('tmux-nav-passthrough', { clear = true }),
+  pattern = 'lazygit',
+  callback = function(ev)
+    local directions = { ['<C-h>'] = 'L', ['<C-j>'] = 'D', ['<C-k>'] = 'U', ['<C-l>'] = 'R' }
+    for key, dir in pairs(directions) do
+      vim.keymap.set({ 'n', 't' }, key, function()
+        vim.fn.system('tmux select-pane -' .. dir)
+      end, { buffer = ev.buf, nowait = true, silent = true })
+    end
+  end,
+})
+
 return {
   "christoomey/vim-tmux-navigator",
   cmd = {
