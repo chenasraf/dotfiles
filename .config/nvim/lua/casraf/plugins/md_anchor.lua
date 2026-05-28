575f8f0851926d28ceaacda02e91f36aea01aa58
Chen Asraf <casraf@pm.me>
2026-05-28 14:56:53 +0300

    refactor(nvim): use lazy for md_anchor local plugin

--- .config/nvim/lua/casraf/plugins/md_anchor.lua
+++ .config/nvim/lua/casraf/plugins/md_anchor.lua
@@ -1,3 +1,8 @@
-require("casraf.lib.md_anchor").setup()
-
-return {}
+return {
+  dir = vim.fn.stdpath("config"),
+  name = "md_anchor",
+  event = "VeryLazy",
+  config = function()
+    require("casraf.lib.md_anchor").setup()
+  end,
+}
