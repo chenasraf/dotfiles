fbeba32fac049830514a4b57afe4929624c26be5
Chen Asraf <casraf@pm.me>
2026-05-31 16:40:50 +0300

    feat(zsh): update hash dirs

--- dirs.zsh
+++ dirs.zsh
@@ -1,16 +1,17 @@
+local is_mac=$(uname -s | grep -q "Darwin")
 hash -d df=~/.dotfiles
 hash -d dl=~/Downloads
-hash -d dotfiles=~/.dotfiles
 hash -d dt=~/Desktop
-hash -d dungeon_paper=~/Dev/dungeon_paper
 hash -d dv=~/Dev
 hash -d emudeck-remote=/Volumes/2T
 hash -d gamedev=~/Dev/redot/games
 hash -d go=~/Dev/go/src/github.com/chenasraf
-hash -d mush=~/Library/Application
 hash -d ncapps=~/Dev/nextcloud-docker-dev/workspace/server/apps-extra
 hash -d notes=~/Nextcloud/Notes
 hash -d ssd=/Volumes/2T
-hash -d steam=~/Library/Application
-hash -d tap=/opt/homebrew/Library/Taps/chenasraf/homebrew-tap
-hash -d alfred=~/Nextcloud/synced/Alfred.alfredpreferences/workflows
+if is_mac; then
+  hash -d tap=/opt/homebrew/Library/Taps/chenasraf/homebrew-tap
+  hash -d alfred=~/Nextcloud/synced/Alfred.alfredpreferences/workflows
+  hash -d apsu="~/Library/Application Support"
+fi
+
