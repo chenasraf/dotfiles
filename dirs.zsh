236618e7e38af4c84f46c46ec0443ae7ced1d7ca
Chen Asraf <casraf@pm.me>
2026-06-03 00:38:27 +0300

    fix(dirs): move local dirs to _local

--- dirs.zsh
+++ dirs.zsh
@@ -1,17 +1,3 @@
 local is_mac=$(uname -s | grep -q "Darwin")
 hash -d df=~/.dotfiles
-hash -d dl=~/Downloads
-hash -d dt=~/Desktop
-hash -d dv=~/Dev
-hash -d emudeck-remote=/Volumes/2T
-hash -d gamedev=~/Dev/redot/games
-hash -d go=~/Dev/go/src/github.com/chenasraf
-hash -d ncapps=~/Dev/nextcloud-docker-dev/workspace/server/apps-extra
-hash -d notes=~/Nextcloud/Notes
-hash -d ssd=/Volumes/2T
-if is_mac; then
-  hash -d tap=/opt/homebrew/Library/Taps/chenasraf/homebrew-tap
-  hash -d alfred=~/Nextcloud/synced/Alfred.alfredpreferences/workflows
-  hash -d apsu=~/Library/Application\ Support
-fi
 
