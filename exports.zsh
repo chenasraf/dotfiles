c53bc9f4b148fbe45f5195d8dab12ac6c0268edd
Chen Asraf <casraf@pm.me>
2026-06-24 10:17:29 +0300

    fix(exports): pnpm dir

--- exports.zsh
+++ exports.zsh
@@ -122,9 +122,10 @@ fi
 # PNPM
 export PNPM_HOME="$HOME/Library/pnpm"
 case ":$PATH:" in
-*":$PNPM_HOME:"*) ;;
-*) export PATH="$PNPM_HOME:$PATH" ;;
+  *":$PNPM_HOME/bin:"*) ;;
+  *) export PATH="$PNPM_HOME/bin:$PATH" ;;
 esac
+# pnpm end
 
 if [[ -f $(which pnpm) ]]; then
   export PATH="$PNPM_HOME:$PATH"
