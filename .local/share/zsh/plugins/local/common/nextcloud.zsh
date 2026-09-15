5fd452ef5643ba30c5b73f830043a9bff9db8b53
Chen Asraf <casraf@pm.me>
2026-09-15 17:08:28 +0300

    feat(nxc): make upgrade update git and images for the dev instance

--- .local/share/zsh/plugins/local/common/nextcloud.zsh
+++ .local/share/zsh/plugins/local/common/nextcloud.zsh
@@ -9,6 +9,7 @@ alias nc-logs="nxc logs --"
 alias nc-pretty-logs="nxc logs --pretty --"
 alias nc-debug="nxc debug --"
 alias nc-install-app="nxc install-app --"
+alias nc-upgrade="nxc upgrade"
 
 # Dev instance
 alias nc-use="nxc use"
@@ -16,7 +17,6 @@ alias nc-start="nxc start"
 alias nc-stop="nxc stop"
 
 # AIO instance
-alias nc-upgrade="nxc upgrade"
 alias nc-upgrade-beta="nxc upgrade --beta"
 alias nc-force-appupdate="nxc force-appupdate"
 alias nc-latest-version="nxc latest-version"
